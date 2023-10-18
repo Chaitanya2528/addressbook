803f7fa3431887e36ca2384950f924dba4b2e253
Chaitanya2528 <chaituveeksha5.marolix@gmail.com>
2023-10-18 23:04:13 +0530

    slave2

---
+++ server_config.sh
@@ -0,0 +1,15 @@
+sudo yum install git -y
+sudo yum install fontconfig java-11-openjdk -y
+sudo yum install maven -y
+
+if [-d "addressbook" ]
+then
+   echo "repo is cloned and exists"
+   cd /home/ec2-user/addressbook
+   git pull origin b1
+else
+   git clone https://github.com/Chaitanya2528/addressbook.git
+   cd addressbook
+   git checkout b1
+fi
+mvn package
